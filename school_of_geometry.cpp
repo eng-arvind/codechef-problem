a8db42a39f38f36a3f51a27be85c6b7df8403af6
Arvind kumar Vishwakarma <41833207+eng-arvind@users.noreply.github.com>
2020-02-11 00:33:58 +0530

    Add files via upload

--- school_of_geometry.cpp
+++ school_of_geometry.cpp
@@ -1,36 +1,32 @@
-#include<iostream>
+#include <iostream>
 #include<bits/stdc++.h>
 #include<algorithm>
-#include<vector>
-#define ll unsigned long long int
 using namespace std;
-int main()
-{
-  int t;
-  cin>>t;
-  while(t--)
-  {
-    long long int  n,i;
-    cin>>n;
-    vector<unsigned long long int> a;
-    for(i=0;i<n;i++)
-    {
-      unsigned long long int k;
-      cin>>k;
-      a.push_back(k);
-    }
-    cout<<a[0]<<endl;
-    ll sum=0;
-    for(i=n-1;i>=0;i--)
-    {
-      unsigned long long int  k;
-      cin>>k;
-      if(k<a[i])
-      sum += k;
-      else
-      sum += a[i];
-      }
-    cout<<sum<<endl;
-  }
-  return 0;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	   long long  int n;
+	    cin>>n;
+	   long long int a[n];
+	    for(int i=0;i<n;i++)
+	    cin>>a[i];
+	    long long int b[n];
+	    for(int i=0;i<n;i++)
+	    cin>>b[i];
+	    sort(a,a+n);
+	    sort(b,b+n);
+	    unsigned long long  int sum=0;
+	    for(int i=0;i<n;i++)
+	    {
+	     if(a[i]<b[i])
+	     sum+=a[i];
+	     else
+	     sum+=b[i];
+	    }
+	    cout<<sum<<endl;
+	}
+	return 0;
 }
