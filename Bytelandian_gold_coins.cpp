efb0b99d36306f7a4cf43c7c82600d0a52ed37fd
Arvind kumar Vishwakarma <41833207+eng-arvind@users.noreply.github.com>
2020-02-27 21:01:53 +0530

    Add files via upload

---
+++ Bytelandian_gold_coins.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstdio>
+#include<map>
+#define ll long long int
+using namespace std;
+map<int, long long> dp;
+ll b_g_c(ll n)
+   {
+    if(n==0)
+    return 0;
+    if(dp[n]!=0)
+    return dp[n];
+    ll x=b_g_c(n/2)+b_g_c(n/3)+b_g_c(n/4);
+
+    if(x>n)
+    dp[n]=x;
+    else
+    dp[n]=n;
+    return dp[n];
+}
+int main()
+{
+    ll n;
+    while(cin>>n) cout<<b_g_c(n)<<endl;
+  return 0;
+}
